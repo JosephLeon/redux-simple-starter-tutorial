efa9c56e24124f0f18eb8c487c08146ad51b93e8
Joseph Leon <joseph.leon.9@gmail.com>
2017-02-04 22:06:06 -0500

    Add const for API key.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+const API_KEY = 'PLACE_YOUR_API_KEY_HERE';
+
 // Create a new component and this component should produce some html
 const App = () => {
 	return <div>Hi!</div>;
