91f363a66d052cc749a6803ab9709208641512f6
Joseph Leon <joseph.leon.9@gmail.com>
2017-02-04 22:20:32 -0500

    Import the search bar component and throw it in our app component, move js6 code inside paranthesis for better readability.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'PLACE_YOUR_API_KEY_HERE';
 
 // Create a new component and this component should produce some html
 const App = () => {
-	return <div>Hi!</div>;
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
 }
 
 // Take this components HTML and put it on the page
