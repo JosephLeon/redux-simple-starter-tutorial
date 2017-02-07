04de99947d1ace467850b9ce26999042e199f8c8
Joseph Leon <joseph.leon.9@gmail.com>
2017-02-06 21:20:15 -0500

    Comment out other function and place function inline to reduce code.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
 	render() {
-		return <input onChange={this.onInputChange} />;
+		return <input onChange={event => console.log(event.target.value)} />;
 	}
 
-	onInputChange(event) {
-		console.log(event.target.value);
-	}
+	// onInputChange(event) {
+	// 	console.log(event.target.value);
+	// }
 }
 
 export default SearchBar;
