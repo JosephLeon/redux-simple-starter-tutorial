1b0427393e5713700039862b2d5cbd7c3e079cba
Joseph Leon <joseph.leon.9@gmail.com>
2017-02-07 21:01:08 -0500

    Add a constructor method and add a property of term for the state, add a function as an event handler onchange that sets the state for term as the inputs value.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-	render() {
-		return <input onChange={event => console.log(event.target.value)} />;
+	constructor(props) {
+		super(props);
+
+		this.state = { term: '' };
 	}
 
-	// onInputChange(event) {
-	// 	console.log(event.target.value);
-	// }
+	render() {
+		return (
+			<div>
+				<input onChange={event => this.setState({ term: event.target.value })} />
+			</div>
+		);
+	}
 }
 
 export default SearchBar;
