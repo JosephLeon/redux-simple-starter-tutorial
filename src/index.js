8609856890e27143db4bfbf05133fe02f3da1cb9
Joseph Leon <joseph.leon.9@gmail.com>
2017-03-15 14:25:27 -0400

    Add selectedVideo to initial state with a value of null, add correct class for the loading fallback, add the first video as selectedVideo to the YTSearch function.

--- src/index.js
+++ src/index.js
@@ -10,11 +10,17 @@ class App extends Component {
 	constructor (props) {
 		super(props);
 
-		this.state = { videos: [] };
+		this.state = { 
+			videos: [],
+			selectedVideo: null
+		};
 
 		YTSearch({key: API_KEY, term: 'dragonball super'}, (videos) => {
 			console.log(videos);
-			this.setState({ videos });
+			this.setState({ 
+				videos: videos,
+				selectedVideo: videos[0]
+			});
 		});
 	}
 
@@ -22,7 +28,7 @@ class App extends Component {
 		return (	
 			<div>
 				<SearchBar />
-				<VideoDetail video={this.state.videos[0]} />
+				<VideoDetail video={this.state.selectedVideo} />
 				<VideoList videos={this.state.videos} />
 			</div>
 		);
