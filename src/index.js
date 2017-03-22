6b8328476ac25514dba5204ee3e3fbe423539fd0
Joseph Leon <joseph.leon.9@gmail.com>
2017-03-22 15:07:59 -0400

    Add import for variables file, set a const API_KEY as the data from variables file.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
+import file from './variables.js';
 // const API_KEY = 'PLACE_YOUR_API_KEY_HERE';
+const API_KEY = file.data;
 
 class App extends Component {
 	constructor (props) {
